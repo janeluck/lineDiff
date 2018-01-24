3dbc1c7da3e987631a12233f4ab4ec6a315e80ab
janeluck <janeluck158@gmail.com>
2018-01-24 18:34:39 +0800

    按行显示

--- index.js
+++ index.js
@@ -4,6 +4,56 @@ import _ from 'lodash'
 
 const fs = require("fs")
 
+
+function lcs(X, Y) {
+    const xLength = X.length, yLength = Y.length, c = [], b = []
+    c[-1] = new Array(yLength).fill(0)
+    for (let i = 0; i < xLength; i++) {
+        b[i] = []
+        c[i] = []
+        c[i][-1] = 0
+        for (let j = 0; j < yLength; j++) {
+            if (X[i] === Y[j]) {
+                c[i][j] = c[i][j - 1] + 1
+                b[i][j] = 0
+            } else {
+                //c[i][j] = Math.max(c[i - 1][j], c[i][j - 1])
+
+                if (c[i - 1][j] > c[i][j - 1]) {
+                    c[i][j] = c[i - 1][j]
+                    b[i][j] = 1
+                } else {
+                    c[i][j] = c[i][j - 1]
+                    b[i][j] = 2
+
+                }
+
+            }
+        }
+    }
+
+
+    return b
+}
+
+
+// 打印最长公共子序列矩阵
+//function printLCS(lcsArr, X, Y, i, j) {
+function printLCS(lcsArr, X, Y, i, j) {
+
+    if (i === -1 || j === -1) {
+        return ''
+    }
+    if (lcsArr[i][j] === 0) {
+        return printLCS(lcsArr, X, Y, i - 1, j - 1) + '\n' + X[i]
+    } else if (lcsArr[i][j] === 1) {
+        return printLCS(lcsArr, X, Y, i - 1, j)
+    } else {
+        return printLCS(lcsArr, X, Y, i, j - 1)
+    }
+}
+
+
 const generateLinesPromise = function (filePath) {
     return new Promise(function (resolve, reject) {
         fs.readFile(filePath, function (err, f) {
@@ -12,9 +62,59 @@ const generateLinesPromise = function (filePath) {
     })
 }
 
+
 async function lineDiff(...args) {
     const arrs = await Promise.all(_.map(args, generateLinesPromise))
+    const X = arrs[0], Y = arrs[1]
+    const LCS = printLCS(lcs(X, Y), X, Y, X.length - 1, X.length - 1).split('\n')
+    let mStart = 0, nStart = 0, result = []
+    for (let i = 0; i < LCS.length; i++) {
+        for (let m = mStart; m < X.length; m++) {
+            if (X[m] === LCS[i]) {
+                X.slice(mStart, m).map(function (str) {
+                    result.push({
+                        'signal': '-',
+                        'str': str
+                    })
+                })
+                mStart = m + 1
+                break
+            }
+        }
 
+        for (let n = nStart; n < Y.length; n++) {
+            if (Y[n] === LCS[i]) {
+                Y.slice(nStart, n).map(function (str) {
+                    result.push({
+                        'signal': '+',
+                        'str': str
+                    })
+                })
+                nStart = n + 1
+                break
+            }
+        }
+        result.push({
+            'signal': '',
+            'str': LCS[i]
+        })
+
+    }
+    if (mStart !== X.length) {
+        result.push({
+            'signal': '-',
+            'str': X.slice(mStart)
+        })
+    }
+    if (nStart !== Y.length) {
+        result.push({
+            'signal': '+',
+            'str': Y.slice(nStart)
+        })
+    }
+    return result.map(function (line) {
+        console.log(line.signal + line.str)
+    })
 }
 
 lineDiff('./a.txt', './b.txt')
