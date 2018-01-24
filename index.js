fbe2cffc99f2695735874763a06c0c8b3b86d0fa
janeluck <janeluck158@gmail.com>
2018-01-24 18:13:25 +0800

    babel

--- index.js
+++ index.js
@@ -1,43 +1,9 @@
 // 按行比较两个文件的不同
 // 以行为单位, 先求得最长公共子序列
+import _ from 'lodash'
 
-/*
+const fs = require("fs")
 
-const lineReader0 = require('readline').createInterface({
-        input: require('fs').createReadStream('./a.txt')
-    }),
-    lineReader1 = require('readline').createInterface({
-        input: require('fs').createReadStream('./b.txt')
-    }), lineArr0 = [], lineArr1 = []
-
-lineReader0.on('line', function (line) {
-    //console.log('Line from file:', line);
-    lineArr0.push(line)
-})
-lineReader1.on('line', function (line) {
-    //console.log('Line from file:', line);
-    lineArr1.push(line)
-})
-
-const  generateLines = function (filePath) {
-    const lineArr = []
-    require('readline').createInterface({
-        input: require('fs').createReadStream(filePath)
-    }).on('line', function (line) {
-        lineArr.push(line)
-    })
-
-    return lineArr
-}
-
-console.log(generateLines('./a.txt'))*/
-var fs = require("fs")
-let result
-fs.readFile('./a.txt', function (err, f) {
-    result = f.toString().split('\n');
-    console.log(result)
-    // use the array
-})
 const generateLinesPromise = function (filePath) {
     return new Promise(function (resolve, reject) {
         fs.readFile(filePath, function (err, f) {
@@ -46,10 +12,8 @@ const generateLinesPromise = function (filePath) {
     })
 }
 
-async function lineDiff(file0, file1) {
-    const arrs = await Promise.all([generateLinesPromise(file0), generateLinesPromise(file1)])
-
-    console.log(...arrs)
+async function lineDiff(...args) {
+    const arrs = await Promise.all(_.map(args, generateLinesPromise))
 
 }
 
